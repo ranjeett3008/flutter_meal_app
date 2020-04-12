c52f80ba5f1e31efa4b6b02e9bf9dd6783181b07
Ranjeet <ranjeet@gmail.com>
2020-04-12 12:49:41 +0530

    screen navigation using named routes

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
 
-  final String categoryTitle;
-  final String categoryId;
+  // final String categoryTitle;
+  // final String categoryId;
 
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+
+    final routeArgs = 
+    ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
