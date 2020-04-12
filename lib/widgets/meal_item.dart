1dd37fde38f329c47c8481fd466611d714916cb0
Ranjeet <ranjeet@gmail.com>
2020-04-12 18:28:22 +0530

    completed meal list item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -18,6 +18,38 @@ class MealItem extends StatelessWidget {
   void selectMeal(){
 
   }
+
+  String get ComplexityText{
+    switch(complexity){
+      case Complexity.Simple:
+      return 'Simple';
+      break;
+      case Complexity.Challenging:
+      return 'Challenging';
+      break;
+      case Complexity.Hard:
+      return 'Hard';
+      break; 
+      default:
+      return 'Unknown';
+    }
+  }
+
+   String get AffordabilityText{
+    switch(affordability){
+      case Affordability.Affordable:
+      return 'Affordable';
+      break;
+      case Affordability.Luxurious:
+      return 'Luxurious';
+      break;
+      case Affordability.Pricey:
+      return 'Pricey';
+      break; 
+      default:
+      return 'Unknown';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return InkWell(onTap: selectMeal,
@@ -33,10 +65,63 @@ class MealItem extends StatelessWidget {
             topLeft: Radius.circular(15),
             topRight: Radius.circular(15)
           ),
-            child: Image.network(imageUrl),
+            child: Image.network(imageUrl,
+             height: 250, 
+             width: double.infinity,
+             fit:BoxFit.cover),
+          ),
+          Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  color: Colors.black54,
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 20,
+                    ),
+                  width: 300,
+                  child: Text(
+                  title, 
+                  style:TextStyle(
+                    fontSize: 26,
+                    color: Colors.white
+                  ),
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+              ),
+                ),
           )
-        ],)
-      ],),
+        ],
+        ),
+        Padding(
+        
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(Icons.schedule),
+                  SizedBox(width: 6,),
+                  Text('$duration min'),
+              ],),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.work),
+                  SizedBox(width: 6,),
+                  Text(ComplexityText),
+              ],),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.attach_money),
+                  SizedBox(width: 6,),
+                  Text(AffordabilityText),
+              ],)
+            ],
+            ),
+          )
+      ],
+      ),
     ),
     );
   }
